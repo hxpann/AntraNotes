5eed838458cb22fbc3a95537f1dcb4c5b18f4475
hxpann <82172887+hxpann@users.noreply.github.com>
2022-04-23 23:54:05 -0400

    update

--- C#/Assignment3/Assignment3/oop.cs
+++ C#/Assignment3/Assignment3/oop.cs
@@ -1,37 +1,42 @@
-namespace Assignment3;
+using System;
+using System.Collections.Generic;
+using System.Text;
 
-// Designing and Building Classes using object-oriented principles
-public class Person
+namespace Assignment3
 {
-    //private instance variables
-    private string name, address;
-
-    //Constructs a Person instance with the given name and address
-    public Person(String name, String address)
+    public class Person
     {
-        this.name = name;
-        this.address = address;
+        //private instance variables
+        private string name, address;
+
+        //Constructs a Person instance with the given name and address
+        public Person(String name, String address)
+        {
+            this.name = name;
+            this.address = address;
+        }
+
+
+        public string Phone { get; set; }
+        public string Email { get; set; }
+
     }
+    public class Student
+    {
 
-    
-    public string Phone { get; set; }
-    public string Email { get; set; }
+    }
+    public class Instructor
+    {
+        public DateTime JoinDate { get; set; }
 
+    }
+    public class Course
+    {
+
+    }
+    public class Department
+    {
+
+    }
 }
-public class Student
-{
-
-}
-public class Instructor
-{
-    public DateTime JoinDate { get; set; }
-
-}
-public class Course
-{
-
-}
-public class Department
-{
-
-}
+// Designing and Building Classes using object-oriented principles
